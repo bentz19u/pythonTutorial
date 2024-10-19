fdadaa733a7d4317efc7b9ad13fc88c2759504e5
Daniel Bentz <db.daniel.bentz@gmail.com>
2024-10-19 16:11:14 +0900

    testing syntax and variable manipulation

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Hello World')
+# This is my first Python program
+# I am just checking the syntax and testing stuff as an introduction
+
+# variables
+fullName = "dear reader"
+age = 35
+gpa = 3.2
+isStudent = False # need to be capitalized
+
+# user f string to insert the variable
+print(f'Hello {fullName}')
+print(f'Your are {age} years old')
+print(f'Your GPA is {gpa}')
+
+# if statement
+if isStudent:
+    print('You are a student')
+else:
+    print('You are NOT a student')
+
+# get the type of variable
+print(type(gpa)) # return <class 'float'>
+
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
---
+++ user-input.py
@@ -0,0 +1,8 @@
+# user input
+name = input('Enter your name: ')
+age = int(input ('Enter your age: '))
+
+age += 1
+
+print(f'Hello {name}!')
+print(f'You are {age} years old!')
