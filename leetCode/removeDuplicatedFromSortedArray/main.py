8d02f6172d6813e86d03f2c31e25ada05eae031a
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-10-13 11:40:24 +0900

    added solution for Remove Duplicates from Sorted Array leetcode problem

---
+++ leetCode/removeDuplicatedFromSortedArray/main.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from jovian.pythondsa import evaluate_test_case
+
+from tests import test, tests
+
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+# The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+#
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+#     Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+#     Return k.
+
+
+"""
+Possible cases
+
+1. small list
+2. large list
+3. empty list
+4. input and output are the same
+"""
+
+
+# first version, it can be improved easily
+def removeDuplicates(nums: List[int]) -> int:
+    numbers_found = []
+    index_to_delete = []
+
+    for index, num in enumerate(nums):
+        if num not in numbers_found:
+            numbers_found.append(num)
+        else:
+            index_to_delete.append(index)
+
+    for index in reversed(index_to_delete):
+        del (nums[index])
+
+    return len(nums)
+
+
+# result = evaluate_test_case(removeDuplicates, test)
+# print(result)
+
+is_all_tests_succeed = True
+time_to_process = 0
+
+for test in tests:
+    result = evaluate_test_case(removeDuplicates, test)
+    time_to_process += result[2]
+
+    # index 1 has a boolean if the test worked or not
+    if not result[1]:
+        is_all_tests_succeed = False
+
+print(f"ALL TESTS SUCCEED? {is_all_tests_succeed}, time to process {time_to_process}")
---
+++ leetCode/removeDuplicatedFromSortedArray/tests.py
@@ -0,0 +1,7 @@
+test = {'input': {'nums': [1, 1, 2]}, 'output': 2}
+
+tests = [{'input': {'nums': [1, 1, 2]}, 'output': 2},
+         {'input': {'nums': [1, 2]}, 'output': 2},
+         {'input': {'nums': []}, 'output': 0},
+         {'input': {'nums': [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]}, 'output': 5}
+         ]
